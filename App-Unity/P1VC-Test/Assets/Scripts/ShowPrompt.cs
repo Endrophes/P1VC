d32737999fd7940856dbfdf5a1f6b3759a9a9fe5
Endrophes <Endrophes@users.noreply.github.com>
2023-10-18 10:06:11 -0400

    Finished the Exam
    I finished the exam with all the required functions.
    
    1. A indirect action via a Collider
    2. A direct action via button press

--- App-Unity/P1VC-Test/Assets/Scripts/ShowPrompt.cs
+++ App-Unity/P1VC-Test/Assets/Scripts/ShowPrompt.cs
@@ -1,38 +1,73 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShowPrompt : MonoBehaviour
 {
-
+    public GameObject spawnLocation;
+    public GameObject objectToSpawn;
     public GameObject uiObject;
-    // Start is called before the first frame update
+    bool _CanGetSoda = false;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     void Start()
     {
-        uiObject.SetActive(false);
+        SetState(false);
     }
 
-    //
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        if (_CanGetSoda)
+        {
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                //Spawn Soda
+                Instantiate(objectToSpawn, spawnLocation.transform.position, spawnLocation.transform.rotation);
+            }
+        }
+    }
+
+    /// <summary>
+    ///  On Enter - Enable system
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            uiObject.SetActive(true);
+            SetState(true);
         }
     }
 
-    //
+    /// <summary>
+    /// On Exit - Disable system
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            uiObject.SetActive(false);
+            SetState(false);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="enable"></param>
+    private void SetState(bool enable)
     {
-        
+        if (uiObject != null)
+        {
+            _CanGetSoda = enable;
+            uiObject.SetActive(enable);
+        }
     }
+
 }
